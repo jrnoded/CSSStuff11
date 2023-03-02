af35061bc6138fa3a491e1b5b37dd194e5835381
Jim Roberts <jrnoded@gmail.com>
2023-03-02 11:43:07 -0500

    Smooth Random Movement

---
+++ Blobs/index.js
@@ -0,0 +1,8 @@
+const blob = document.getElementById("blob");
+
+setInterval(moveBlob, 2000);
+
+function moveBlob() {
+    blob.style.top = Math.floor(Math.random()*90+1)+ '%';
+    blob.style.left = Math.floor(Math.random()*90+1)+'%';
+}
