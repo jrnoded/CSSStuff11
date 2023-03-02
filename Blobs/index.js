fce540cd089529f4525d8fb3c4aab5499536b3d3
Jim Roberts <jrnoded@gmail.com>
2023-03-02 11:52:39 -0500

    Add rotation

--- Blobs/index.js
+++ Blobs/index.js
@@ -5,4 +5,5 @@ setInterval(moveBlob, 2000);
 function moveBlob() {
     blob.style.top = Math.floor(Math.random()*90+1)+ '%';
     blob.style.left = Math.floor(Math.random()*90+1)+'%';
+    blob.style.transform = "rotate(" + Math.floor(Math.random()*350 ) + "deg)"
 }
