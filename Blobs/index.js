a89800895ad10e658ef4ccc73515ce31e6f2674c
Jim Roberts <jrnoded@gmail.com>
2023-03-03 13:02:07 -0500

    Add display of location

--- Blobs/index.js
+++ Blobs/index.js
@@ -1,9 +1,14 @@
 const blob = document.getElementById("blob");
+const where = document.getElementById("where");
 
 setInterval(moveBlob, 2000);
 
 function moveBlob() {
-    blob.style.top = Math.floor(Math.random()*90+1)+ '%';
-    blob.style.left = Math.floor(Math.random()*90+1)+'%';
-    blob.style.transform = "rotate(" + Math.floor(Math.random()*350 ) + "deg)"
+  var top = `${Math.floor(Math.random() * 90 + 1)}%`;
+  var left = `${Math.floor(Math.random() * 90 + 1)}%`;
+  var rot = `rotate(${Math.floor(Math.random() * 360)}deg)`;
+  blob.style.top = `${top}`;
+  blob.style.left = `${left}`;
+  blob.style.transform = `${rot}`;
+  where.innerText = `top: ${top} left: ${left} ${rot} `
 }
